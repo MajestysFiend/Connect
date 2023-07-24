a6edf868518f8e4df69cfe65b94ed30ace4f4c02
Tristan Lewis <94727522+MajestysFiend@users.noreply.github.com>
2023-07-24 09:02:03 -0400

    Integrated Atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('f5430ad84fb8462cb8e89b340d72d380').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
