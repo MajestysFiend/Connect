3a31e1af34e96016c32a48a420811124fea53c24
Tristan Lewis <94727522+MajestysFiend@users.noreply.github.com>
2023-07-29 10:09:31 -0400

    Added alert and offline functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 import './App.css';
 import { useState, useEffect } from 'react';
 import { extractLocations, getEvents } from './api';
@@ -13,6 +13,7 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState();
+  const [warningAlert, setWarningAlert] = useState("");
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -28,16 +29,24 @@ const App = () => {
   }
 
   useEffect(() => {
+    let warningMessage
+    if (navigator.onLine) {
+      warningMessage = ""
+    } else {
+      warningMessage = "The displayed list has been loaded from your cache"
+    }
+    setWarningAlert(warningMessage);
     fetchData();
   }, [currentCity, currentNOE]);
 
   console.log("currentNOE: ", currentNOE);
-  
+
   return (
     <div className="App">
-      <div className="alerts-container">
+      <div className="alerts-container" setWarningAlert={setWarningAlert}>
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <h1>Connect</h1>
       <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
